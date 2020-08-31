b7b5802b281c35261435f70a671c527961f07158
Adipta Biswas <40853486+AdiptaBiswas@users.noreply.github.com>
2020-08-31 09:16:08 +0530

    Add files via upload

---
+++ Problem - 5.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+def lowest_multiple():
+    end = 20
+    n1 = 11
+    n2 = n1 + 1
+    number = 0
+    while n2 <= end:
+        n1 = lcm(n1,n2)
+        n2 += 1
+    print(n1)
+
+
+# In[9]:
+
+
+def lcm(n1,n2):
+    if n1 > n2:
+        large = n1
+    else:
+        large = n2
+    while True:
+        if (large % n2 == 0) and (large % n1 == 0):
+            lcm = large
+            break
+        else:
+            large += 1    
+    return lcm   
+
+
+# In[10]:
+
+
+lowest_multiple()
+
+
+# In[ ]:
+
+
+
+
